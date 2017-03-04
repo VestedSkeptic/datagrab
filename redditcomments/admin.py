d9750def0642e2298103a12439aed225ff65e503
Mike Harley <VestedSkeptic@gmail.com>
2017-03-04 11:11:57 -0500

    x

--- redditcomments/admin.py
+++ redditcomments/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import redditcomment
 
-# Register your models here.
+admin.site.register(redditcomment)
--- redditcomments/models.py
+++ redditcomments/models.py
@@ -5,13 +5,12 @@ from redditusers.models import reddituser
 
 # Create your models here.
 class redditcomment(models.Model):
-    # username = models.CharField(max_length=30)
     user = models.ForeignKey(
      reddituser,
      on_delete=models.CASCADE,
-    )    
-    
-    
-    
+    )
+
+
+
     # def __str__(self):
-    #     return format(self.user.username) 
+    #     return format(self.user.username)
