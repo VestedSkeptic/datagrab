d15ff1f6895b11b129723a71a5886b224c8d00b2
Mike Harley <VestedSkeptic@gmail.com>
2017-03-05 11:40:56 -0500

    x

--- reddit/models.py
+++ reddit/models.py
@@ -4,19 +4,15 @@ from .defines import *
 
 # *****************************************************************************
 class user(models.Model):
-    name = models.CharField(max_length=30)
+    name    = models.CharField(max_length=30)
     def __str__(self):
         return format(self.name)
 
 # *****************************************************************************
 class userCommentsProcessedStatus(models.Model):
-    # user = models.ForeignKey(user, on_delete=models.CASCADE, primary_key=True)
-    # user = models.ForeignKey(user, on_delete=models.CASCADE, unique=True)
-    user = models.OneToOneField(user, primary_key=True)
-
-    # after = models.CharField(max_length=30, default=CONST_UNPROCESSED, blank=True, null=True)
-    after  = models.CharField(max_length=30, default=CONST_UNPROCESSED)
-    before = models.CharField(max_length=30, default=CONST_UNPROCESSED)
+    user    = models.OneToOneField(user, primary_key=True)
+    after   = models.CharField(max_length=30, default=CONST_UNPROCESSED)
+    before  = models.CharField(max_length=30, default=CONST_UNPROCESSED)
     def __str__(self):
         s = self.user.name
         s += " [After: " + self.after + "]"
