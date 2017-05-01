baf812db91cd086e2dc5ca8a55718cfea5b8b759
Mike Harley <VestedSkeptic@gmail.com>
2017-05-01 16:54:34 -0400

    Cleanup

--- rMain/models.py
+++ rMain/models.py
@@ -1,6 +1,4 @@
-# from __future__ import unicode_literals
-# from django.db import models
-# from redditCommon.constants import *
+
 
 
 
--- rSubredditThreads/views.py
+++ rSubredditThreads/views.py
@@ -1,17 +1,6 @@
 from django.http import HttpResponse
 from .threads import threads_updateForAllSubreddits
 
-# # *****************************************************************************
-# def index(request):
-#     s  = ''
-#     s += '<br><a href="http://localhost:8000/admin/">admin</a>'
-#     s += '<br>'
-#     s += '<br><a href="http://localhost:8000/reddit/uc/">update users comments</a>'
-#     s += '<br>'
-#     s += '<br><a href="http://localhost:8000/reddit/ut/">update subreddit threads</a>'
-#     s += '<br>'
-#     return HttpResponse(s)
-
 # *****************************************************************************
 def updateSubredditThreads(request):
     s = threads_updateForAllSubreddits()
