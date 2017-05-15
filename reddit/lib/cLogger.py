713aa9c1d63fc29be4c351aa37b265ebb02e8f37
Mike Harley <VestedSkeptic@gmail.com>
2017-05-15 12:15:22 -0400

    cLogger now has second TRACE file hander. File handlers can optionally archive older log files by renaming them with sequential numbers.

--- reddit/lib/cLogger.py
+++ reddit/lib/cLogger.py
@@ -3,7 +3,8 @@ import logging
 import datetime
 import time
 import inspect
-import pprint
+import os
+# import pprint
 
 cLoggerLevel_CRITICAL   = logging.CRITICAL
 cLoggerLevel_ERROR      = logging.ERROR
@@ -13,6 +14,9 @@ cLoggerLevel_DEBUG      = logging.DEBUG
 cLoggerLevel_TRACE      = 9
 cLoggerLevel_NOTSET     = logging.NOTSET
 
+cLoggerArchive_True     = True
+cLoggerArchive_False    = False
+
 # *****************************************************************************
 class cLogger(object):
 
@@ -20,8 +24,7 @@ class cLogger(object):
     def __init__(self, name):
         self.name = name
         self.baseLoggerLevel        = logging.CRITICAL
-        self.consoleHandlerLevel    = logging.NOTSET
-        self.fileHandlerLevel       = logging.NOTSET
+        self.handlerLevels          = {}
         self.loggerInfoLevel        = logging.DEBUG
         self.methodInfoLevel        = logging.DEBUG
         self.logger = logging.getLogger(self.name)
@@ -29,10 +32,10 @@ class cLogger(object):
         self.funcdict = {}
 
     # --------------------------------------------------------------------------
-    def addConsoleLogger(self, loggingLevel):
+    def addConsoleLogger(self, loggingLevel, name):
         handler = logging.StreamHandler()
         handler.setLevel(loggingLevel)
-        self.consoleHandlerLevel= loggingLevel
+        self.handlerLevels[name]=loggingLevel
         formatter = logging.Formatter(self.getFormatString(), datefmt='%H:%M:%S')
         formatter.converter = time.localtime
         handler.setFormatter(formatter)
@@ -42,10 +45,53 @@ class cLogger(object):
             self.logger.setLevel(self.baseLoggerLevel)
 
     # --------------------------------------------------------------------------
-    def addFileLogger(self, filePathAndName, loggingLevel):
-        handler = logging.FileHandler(filePathAndName, mode='w', encoding=None, delay=False)
+    def getArchiveFilename(self, fNameKey, archiveIndex, fNameType):
+        return "%s%03d.%s" % (fNameKey, archiveIndex, fNameType)
+
+    # --------------------------------------------------------------------------
+    def archiveOlderVersions(self, pathFileName):
+        # get path and filename
+        pathFileTuple = pathFileName.rpartition('/')    # if success: path = pathFileTuple[0], filename = pathFileTuple[2]
+        # get fname and ftype
+        nameTypeTuple = pathFileTuple[2].partition('.') # if success: fname = nameTypeTuple[0], type = nameTypeTuple[2]
+
+        if not (pathFileTuple[0] and pathFileTuple[1] and nameTypeTuple[0] and nameTypeTuple[1]):
+            self.logger.critical("Error splitting %s into path and file and or %s into name and type" % (pathFileName, pathFileTuple[2]))
+        else:
+            fNameKey    = nameTypeTuple[0]         # if success: fNameKey = element 0
+            fNameType   = nameTypeTuple[2]         # if success: fNameType = element 2
+            # get names of all previously archived files
+            fileList = []
+            for fname in os.listdir(pathFileTuple[0]):
+                if fname.startswith(fNameKey):
+                    fileList.append(fname)
+            # remove base filename if in list
+            if (pathFileTuple[2] in fileList):
+                fileList.remove(pathFileTuple[2])
+            # reverse sort fileList
+            fileList.sort(reverse=True)
+            # len(fileList) = number of archived files found.
+            # therefore increment by one for archive_index for renaming
+            archiveIndex = len(fileList) + 1
+            # fileList is sorted with oldest names first: ex log_004.txt, log_003.txt, log_002.txt, log_001.txt
+            # for each rename with older archiveIndex
+            for x in fileList:
+                archiveFilename = self.getArchiveFilename(fNameKey, archiveIndex, fNameType)
+                # self.logger.critical("%s to be renamed to %s" % (x, archiveFilename))
+                archiveIndex -= 1
+                os.rename(pathFileTuple[0]+'/'+x, pathFileTuple[0]+'/'+archiveFilename)
+            # finally rename newest log file
+            archiveFilename = self.getArchiveFilename(fNameKey, archiveIndex, fNameType)
+            if (os.path.isfile(pathFileName)):
+                os.rename(pathFileName, pathFileTuple[0]+'/'+archiveFilename)
+
+    # --------------------------------------------------------------------------
+    def addFileLogger(self, pathFileName, loggingLevel, name, archiveType):
+        if archiveType==cLoggerArchive_True:
+            self.archiveOlderVersions(pathFileName)
+        handler = logging.FileHandler(pathFileName, mode='w', encoding=None, delay=False)
         handler.setLevel(loggingLevel)
-        self.fileHandlerLevel= loggingLevel
+        self.handlerLevels[name]=loggingLevel
         formatter = logging.Formatter(self.getFormatString(), datefmt='%H:%M:%S')
         formatter.converter = time.localtime
         handler.setFormatter(formatter)
@@ -99,12 +145,11 @@ class cLogger(object):
         mPtr("%-24s: %s" % ("Initialized", self.name))
         mPtr("%-24s: %s" % ("Time", str(now)))
         mPtr("%-24s: %s" % ("Base Logger Level", logging.getLevelName(self.baseLoggerLevel)))
-        mPtr("%-24s: %s" % ("Console Handler Level", logging.getLevelName(self.consoleHandlerLevel)))
-        mPtr("%-24s: %s" % ("File Handler Level", logging.getLevelName(self.fileHandlerLevel)))
+        for k in self.handlerLevels:
+            mPtr("%-24s: %s" % (k, logging.getLevelName(self.handlerLevels[k])))
         mPtr("%-24s: %s" % ("Logger Info Level", logging.getLevelName(self.loggerInfoLevel)))
         mPtr("%-24s: %s" % ("Method Info Level", logging.getLevelName(self.methodInfoLevel)))
         mPtr("************************************************************")
-        mPtr("")
         return
 
     # --------------------------------------------------------------------------
@@ -123,14 +168,17 @@ class cLogger(object):
         methodName = inspect.stack()[1][3]
         fileName = inspect.stack()[1][1].rpartition('/')[2] # right partition string by backslash and return third part of tuple
 
-        mPtr("-------------------------------------")
-        mPtr("%-15s: %s" % ("FileName", fileName))
-        mPtr("%-15s: %s" % ("MethodName", methodName))
+        os = "%-20s: " % (fileName + "." + methodName + "()")
         args, _, _, values = inspect.getargvalues(inspect.stack()[1][0])
+        firstIteration = True
         for i in args:
-            mPtr("%-15s: %s" % (i, values[i]))
+            if firstIteration: os += "("
+            else: os += ", "
+            firstIteration = False
+            os += "%s=%s" % (i, values[i])
+        if not firstIteration: os += ")"
+        mPtr(os)
 
-        # return fileName + " - " + methodName + "(): "
         return methodName + "(): "
 
 
